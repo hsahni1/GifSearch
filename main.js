fbdf2f95e35b348012ed0721e3b4683f92d8b856
hsahni1 <harnika.sjsu@gmail.com>
2018-07-31 12:41:09 -0700

    create main.js

---
+++ main.js
@@ -0,0 +1,87 @@
+   var apikey = 'dc6zaTOxFJmzC';
+
+    $(document).ready(function() {
+      /* 
+      * The following two functions are used for making the API call using
+      * pure Javascript. I wouldn't worry about the details
+      */
+
+      function encodeQueryData(data)
+      {
+         var ret = [];
+         for (var d in data)
+            ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+         return ret.join("&");
+      }
+
+      function httpGetAsync(theUrl, callback)
+      {
+          var xmlHttp = new XMLHttpRequest();
+          xmlHttp.onreadystatechange = function() { 
+              if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                  callback(xmlHttp.responseText);
+          }
+          xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+          xmlHttp.send(null);
+      }
+
+
+      /*
+      * The following functions are what do the work for retrieving and displaying gifs
+      * that we search for.
+      */
+    function getGif(query) {
+    console.log(query);
+        query = query.replace(' ', '+');
+        var params = { 'api_key': apikey, 'q': query};
+        params = encodeQueryData(params);
+
+          httpGetAsync('http://api.giphy.com/v1/gifs/search?' + params, function(data) {
+              
+                var gifs = JSON.parse(data);
+                var target = $('#image')
+                for(var x=0; x<gifs.data.length-16; x++){
+                var showgif = gifs.data[x].images.fixed_width.url;
+                $("#image"+(x+1)).html("<img src='" + showgif + "'>");
+        };
+
+
+      })
+      }
+
+        function getTrendingGif() {
+        var params = { 'api_key': apikey};
+
+          httpGetAsync('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC', function(data) {
+    
+                var gifs = JSON.parse(data);
+                var target = $('#image');
+                for(var x=0; x<gifs.data.length-16; x++){
+                var showgif = gifs.data[x].images.fixed_width.url;
+                $("#image"+(x+1)).html("<img src='" + showgif + "'>");
+       };
+      })
+      }
+               
+      $("#submitButton").on("click", function() {
+          var query = $("#inputQuery").val()
+          if(query==""){
+                 $("p").html("OOPS! Enter GIF");
+             }
+          else{
+            $("p").html("TAADDAAAA!!");
+            $(".grid-container").css("display","grid")
+            getGif(query);  
+          }
+
+      });
+          $("#clearButton").on("click", function() {
+        location.reload();
+      });
+        
+    $("#trending").on("click", function() {
+        $("p").html("Trending GIFs!!");
+        $(".grid-container").css("display","grid")
+        getTrendingGif();   
+      });
+    })
